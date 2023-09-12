2ba0c240e3a09527063127908909da16df9266fa
Amirabbas Fazelinia <utilyre@proton.me>
2023-09-12 11:31:37 +0330

    resort to html if public file not exist

--- internal/handler/static.go
+++ internal/handler/static.go
@@ -1,12 +1,35 @@
 package handler
 
 import (
+	"errors"
+	"io/fs"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+type HTMLDir struct {
+	dir http.Dir
+}
+
+func (d HTMLDir) Open(name string) (http.File, error) {
+	f, err := d.dir.Open(name)
+	if err != nil {
+		switch {
+		case errors.Is(err, fs.ErrNotExist):
+			return d.dir.Open(name + ".html")
+		default:
+			return nil, err
+		}
+	}
+
+	return f, nil
+}
+
 func Static(r *mux.Router) {
-	fs := http.FileServer(http.Dir("public"))
+	fs := http.FileServer(HTMLDir{
+		dir: http.Dir("public"),
+	})
+
 	r.PathPrefix("/").Handler(fs)
 }
