2d5244377241a325e23d748c5102d2c6b9b6423d
Amirabbas Fazelinia <utilyre@proton.me>
2023-09-13 16:57:11 +0330

    /chat page

--- internal/handler/static.go
+++ internal/handler/static.go
@@ -2,8 +2,11 @@ package handler
 
 import (
 	"errors"
+	"html/template"
 	"io/fs"
+	"log/slog"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -26,10 +29,40 @@ func (d HTMLDir) Open(name string) (http.File, error) {
 	return f, nil
 }
 
-func Static(r *mux.Router) {
-	fs := http.FileServer(HTMLDir{
-		dir: http.Dir("public"),
-	})
+type staticHandler struct {
+	logger *slog.Logger
+	tmpl   *template.Template
+}
 
-	r.PathPrefix("/").Handler(fs)
+func Static(
+	r *mux.Router,
+	logger *slog.Logger,
+	tmpl *template.Template,
+) {
+	h := staticHandler{
+		logger: logger,
+		tmpl:   tmpl,
+	}
+
+	r.HandleFunc("/chat/{id:[0-9]+}", h.chat).
+		Methods(http.MethodGet)
+
+	r.PathPrefix("/").
+		Handler(http.FileServer(HTMLDir{dir: http.Dir("public")}))
+}
+
+func (h staticHandler) chat(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		h.logger.Warn("failed to convert id URL parameter to integer", "error", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusOK)
+	if err := h.tmpl.ExecuteTemplate(w, "chat", id); err != nil {
+		h.logger.Warn("failed to write body to response", "error", err)
+	}
 }
